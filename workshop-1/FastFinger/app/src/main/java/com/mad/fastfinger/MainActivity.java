148ea309e0e18f61c77562f18c906fc20be817d2
Marek Macko <macko.marek93@gmail.com>
2018-05-09 22:53:18 +0200

    FastFinger - counting clicks (#2)

--- workshop-1/FastFinger/app/src/main/java/com/mad/fastfinger/MainActivity.java
+++ workshop-1/FastFinger/app/src/main/java/com/mad/fastfinger/MainActivity.java
@@ -1,13 +1,61 @@
 package com.mad.fastfinger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String SCORE_KEY = "SCORE_KEY";
+	private int score;
+	private Button scoreButton;
+	private TextView scoreTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init(savedInstanceState);
+	}
+
+	private void init(Bundle savedInstanceState) {
+		initViews();
+		readScoreFromSavedState(savedInstanceState);
+		setupScoreButton();
+	}
+
+	private void initViews() {
+		scoreButton = findViewById(R.id.scoreButton);
+		scoreTextView = findViewById(R.id.scoreTextView);
+	}
+
+	private void readScoreFromSavedState(Bundle savedInstanceState) {
+		if (savedInstanceState != null) {
+			score = savedInstanceState.getInt(SCORE_KEY);
+		}
+	}
+
+	private void setupScoreButton() {
+		setCurrentScoreToTextView();
+		scoreButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				score++;
+				setCurrentScoreToTextView();
+			}
+		});
+	}
+
+	private void setCurrentScoreToTextView() {
+		String currentScoreText = String.valueOf(score);
+		scoreTextView.setText(currentScoreText);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(SCORE_KEY, score);
 	}
 }
