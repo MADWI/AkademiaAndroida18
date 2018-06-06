4afa3c2186dc0864f792866aa0213f80821bc8e7
Marek Macko <marek.macko@intive.com>
2018-06-06 22:25:45 +0200

    Reading and sending messages between activities

--- workshop-3/IntentExplorer/app/src/main/java/com/mad/intentexplorer/DetailsActivity.java
+++ workshop-3/IntentExplorer/app/src/main/java/com/mad/intentexplorer/DetailsActivity.java
@@ -2,10 +2,20 @@ package com.mad.intentexplorer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+	private static final String userName = "Alice";
+	private TextView messageSenderView;
+	private TextInputEditText messageInputView;
+	private TextView incomingMessageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +25,65 @@ public class DetailsActivity extends AppCompatActivity {
 	}
 
 	private void init() {
-		String message = getMessageFromIntent();
-		TextView messageView = findViewById(R.id.message_view);
-		messageView.setText(message);
+		setupBarTitle();
+		initViews();
+		initSendButton();
+		setupIncomingViews();
 	}
 
-	private String getMessageFromIntent() {
+	private void setupBarTitle() {
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setTitle(userName);
+		}
+	}
+
+	private void initViews() {
+		messageInputView = findViewById(R.id.message_input);
+		messageSenderView = findViewById(R.id.message_sender);
+		incomingMessageView = findViewById(R.id.incoming_message);
+	}
+
+	private void initSendButton() {
+		Button sendButton = findViewById(R.id.send_button);
+		sendButton.setOnClickListener(this);
+	}
+
+	private void setupIncomingViews() {
 		Intent intent = getIntent();
 		if (intent == null) {
-			return null;
+			return;
 		}
-		return intent.getStringExtra(MainActivity.MESSAGE_KEY);
+		String message = intent.getStringExtra(MainActivity.MESSAGE_KEY);
+		incomingMessageView.setText(message);
+		String messageSenderLabel = getMessageSenderLabelFromIntent(intent);
+		messageSenderView.setText(messageSenderLabel);
+	}
+
+	@NonNull
+	private String getMessageSenderLabelFromIntent(@NonNull Intent intent) {
+		String messageSender = intent.getStringExtra(MainActivity.MESSAGE_SENDER_KEY);
+		return getString(R.string.message_sender_format, messageSender);
+	}
+
+	@Override
+	public void onClick(View v) {
+		finish();
+	}
+
+	@Override
+	public void finish() {
+		Intent intent = getResultIntent();
+		setResult(RESULT_OK, intent);
+		super.finish();
+	}
+
+	@NonNull
+	private Intent getResultIntent() {
+		String message = messageInputView.getText().toString();
+		Intent intent = new Intent();
+		intent.putExtra(MainActivity.MESSAGE_KEY, message);
+		intent.putExtra(MainActivity.MESSAGE_SENDER_KEY, userName);
+		return intent;
 	}
 }
--- workshop-3/IntentExplorer/app/src/main/java/com/mad/intentexplorer/MainActivity.java
+++ workshop-3/IntentExplorer/app/src/main/java/com/mad/intentexplorer/MainActivity.java
@@ -2,35 +2,101 @@ package com.mad.intentexplorer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.Group;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 	public static final String MESSAGE_KEY = "message_key";
+	public static final String MESSAGE_SENDER_KEY = "message_sender_key";
+	private static final String userName = "Bob";
+	private static final int REQUEST_CODE = 123;
 	private TextInputEditText messageInputView;
+	private TextView incomingMessageView;
+	private TextView messageSenderView;
+	private Group incomingViewsGroup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		initViews();
+		init();
 	}
 
-	private void initViews() {
-		messageInputView = findViewById(R.id.message_input_view);
+	private void init() {
+		setupBarTitle();
+		bindViews();
+		hideIncomingViews();
+		initSendButton();
+	}
+
+	private void setupBarTitle() {
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setTitle(userName);
+		}
+	}
+
+	private void bindViews() {
+		messageSenderView = findViewById(R.id.message_sender);
+		incomingMessageView = findViewById(R.id.incoming_message);
+		incomingViewsGroup = findViewById(R.id.incoming_group);
+		messageInputView = findViewById(R.id.message_input);
+	}
+
+	private void hideIncomingViews() {
+		incomingViewsGroup.setVisibility(View.GONE);
+	}
+
+	private void showIncomingViews() {
+		incomingViewsGroup.setVisibility(View.VISIBLE);
+	}
+
+	private void initSendButton() {
 		Button sendButton = findViewById(R.id.send_button);
 		sendButton.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
+		Intent intent = getIntentWithMessageData();
+		startActivityForResult(intent, REQUEST_CODE);
+		messageInputView.getText().clear();
+	}
+
+	@NonNull
+	private Intent getIntentWithMessageData() {
 		Intent intent = new Intent(this, DetailsActivity.class);
 		String message = messageInputView.getText().toString();
 		intent.putExtra(MESSAGE_KEY, message);
-		startActivity(intent);
+		intent.putExtra(MESSAGE_SENDER_KEY, userName);
+		return intent;
+	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+			setupIncomingViewsFromIntent(data);
+			showIncomingViews();
+		}
+	}
+
+	private void setupIncomingViewsFromIntent(Intent data) {
+		String message = data.getStringExtra(MESSAGE_KEY);
+		String messageSenderLabel = getMessageSenderLabelFromIntent(data);
+		messageSenderView.setText(messageSenderLabel);
+		incomingMessageView.setText(message);
+	}
+
+	@NonNull
+	private String getMessageSenderLabelFromIntent(Intent data) {
+		String messageSender = data.getStringExtra(MESSAGE_SENDER_KEY);
+		return getString(R.string.message_sender_format, messageSender);
 	}
 }
